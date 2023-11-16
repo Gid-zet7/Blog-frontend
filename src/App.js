9f1d359ad9bc99f2598ecdb854b00e67e63ad1c4
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-16 23:58:58 +0000

    Added a login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}></Route>
       <Route index element={<Public />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 }
