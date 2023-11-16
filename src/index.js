4b8a1a6c56dd6c53fac0d1da9762b880cc476225
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-16 23:25:25 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowerRouter, Routes, Route } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowerRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowerRouter>
+    <App />
+  </React.StrictMode>
+);
