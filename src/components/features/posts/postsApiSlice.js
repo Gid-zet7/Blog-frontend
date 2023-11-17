e30980424d0d778422f527f990824a59a03e79d8
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-17 10:57:47 +0000

    Added postApiSlice and usersApiSlice to handle data from the backend

---
+++ src/components/features/posts/postsApiSlice.js
@@ -0,0 +1,52 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../../app/api/apiSlice";
+
+const postsAdapter = createEntityAdapter({});
+
+const initialSate = postsAdapter.getInitialState();
+
+export const postsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => ({
+        url: "/posts",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
+      transformResponse: (responseData) => {
+        const loadedPosts = responseData.map((post) => {
+          post.id = post._id;
+          return post;
+        });
+        return postsAdapter.setAll(initialSate, loadedPosts);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "Post", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "Post", id })),
+          ];
+        } else return [{ type: "Post", id: "LIST" }];
+      },
+    }),
+  }),
+});
+
+export const { useGetPostsQuery } = postsApiSlice;
+
+export const selectPostsResult = postsApiSlice.endpoints.getPosts.select();
+
+// creates memoized selector
+const selectUsersData = createSelector(
+  selectPostsResult,
+  (postsResult) => postsResult.data //   normalized state object with ids & entities
+);
+
+// getSelectors creates these selectors and rename them with aliases using destructuring
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostsById,
+  selectIds: selectPostIds,
+  //   Pass in a selector that returrns the users slice of state
+} = postsAdapter.getSelectors((state) => selectUsersData(state) ?? initialSate);
--- src/components/features/users/usersApiSlice.js
+++ src/components/features/users/usersApiSlice.js
@@ -4,3 +4,49 @@ import { apiSlice } from "../../../app/api/apiSlice";
 const usersAdapter = createEntityAdapter({});
 
 const initialSate = usersAdapter.getInitialState();
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => ({
+        url: "/users",
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
+      transformResponse: (responseData) => {
+        const loadedUsers = responseData.map((user) => {
+          user.id = user._id;
+          return user;
+        });
+        return usersAdapter.setAll(initialSate, loadedUsers);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "User", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "User", id })),
+          ];
+        } else return [{ type: "User", id: "LIST" }];
+      },
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = usersApiSlice;
+
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+
+// creates memoized selector
+const selectUsersData = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult.data //   normalized state object with ids & entities
+);
+
+// getSelectors creates these selectors and rename them with aliases using destructuring
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUsersById,
+  selectIds: selectUserIds,
+  //   Pass in a selector that returrns the users slice of state
+} = usersAdapter.getSelectors((state) => selectUsersData(state) ?? initialSate);
