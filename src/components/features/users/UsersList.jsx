4c084edcb89368ef8b8adf4adc35b8ea7893f0c1
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-17 12:11:59 +0000

    Added users list to display users in a table form

--- src/components/features/users/UsersList.jsx
+++ src/components/features/users/UsersList.jsx
@@ -1,5 +1,51 @@
+import { useGetUsersQuery } from "./usersApiSlice";
+import { PulseLoader } from "react-spinners";
+import User from "./User";
+
 const UsersList = () => {
-  return <h1>Post Lists will be here</h1>;
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery(undefined, {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <PulseLoader color={"#BADA55"} />;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message} </p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = users;
+
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
+
+    // console.log(tableContent)
+
+    content = (
+      <table className="table table--users">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Roles</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+  return content;
 };
 
 export default UsersList;
