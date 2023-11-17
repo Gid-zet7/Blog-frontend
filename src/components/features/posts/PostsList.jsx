3927e29e818cc946c87eddf9c45d97d26ad2e226
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-17 20:00:15 +0000

    Added postList component to handle display of the blog posts

--- src/components/features/posts/PostsList.jsx
+++ src/components/features/posts/PostsList.jsx
@@ -1,5 +1,41 @@
+import { useGetPostsQuery } from "./postsApiSlice";
+import Post from "./Post";
+import { PulseLoader } from "react-spinners";
+
 const PostsList = () => {
-  return <h1>Post Lists will be here</h1>;
+  const {
+    data: posts,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsQuery("postsList", {
+    pollingInterval: 15000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <PulseLoader color={"#BADA55"} />;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message} </p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = posts;
+
+    console.log(ids);
+
+    const postContent = ids?.length
+      ? ids.map((postId) => <Post key={postId} postId={postId} />)
+      : null;
+
+    content = <div> {postContent} </div>;
+
+    return content;
+  }
 };
 
 export default PostsList;
