10c6dd0cb2fba9c42d3199b7cc315eb0fcceb753
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-18 23:41:22 +0000

    Added routes to the edit page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import PostsList from "./components/features/posts/PostsList";
 import UsersList from "./components/features/users/UsersList";
 import NewPost from "./components/features/posts/NewPost";
 import NewUserForm from "./components/features/users/NewUserForm";
+import EditUser from "./components/features/users/EditUser";
+import EditPost from "./components/features/posts/EditPost";
 
 function App() {
   return (
@@ -23,11 +25,13 @@ function App() {
           <Route path="posts">
             <Route index element={<PostsList />} />
             <Route path="new" element={<NewPost />} />
+            <Route path=":id" element={<EditPost />} />
           </Route>
 
           <Route path="users">
             <Route index element={<UsersList />} />
             <Route path="new" element={<NewUserForm />} />
+            <Route path=":id" element={<EditUser />} />
           </Route>
         </Route>
         {/* End of dash route */}
