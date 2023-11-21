d2ef92872794d8545f93583ad285d43a5be120a8
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-21 14:48:34 +0000

    Updated Welcome component to display buttons based on user status

--- src/components/features/auth/Welcome.jsx
+++ src/components/features/auth/Welcome.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 const Welcome = () => {
+  const { Username, isAdmin } = useAuth();
+
   const date = new Date();
   const today = new Intl.DateTimeFormat("en-Us", {
     dateStyle: "full",
@@ -11,7 +14,7 @@ const Welcome = () => {
     <section className="welcome">
       <p>{today}</p>
 
-      <h1>Welcome user!, I hope you are having a good time?</h1>
+      <h1>Welcome {Username}, I hope you are having a good time?</h1>
 
       <p>
         <Link to="/dash/posts">View posts</Link>
@@ -19,12 +22,17 @@ const Welcome = () => {
       <p>
         <Link to="/dash/posts/new">Add new Post</Link>
       </p>
-      <p>
-        <Link to="/dash/users/new">Add new user</Link>
-      </p>
-      <p>
-        <Link to="/dash/users">user settings</Link>
-      </p>
+      {isAdmin && (
+        <p>
+          <Link to="/dash/users/new">Add new user</Link>
+        </p>
+      )}
+
+      {isAdmin && (
+        <p>
+          <Link to="/dash/users">user settings</Link>
+        </p>
+      )}
     </section>
   );
   return content;
