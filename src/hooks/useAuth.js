d04677ea5ca29a0cecfa30d9650cb6b7d23673f9
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-21 14:42:48 +0000

    Added a useAuth hook to extract user info from the token received

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+import { selectCurrentToken } from "../components/features/auth/authSlice";
+import { jwtDecode } from "jwt-decode";
+
+const useAuth = () => {
+  const token = useSelector(selectCurrentToken);
+  let isAdmin = false;
+  let isAuthor = false;
+  let status = "User";
+
+  if (token) {
+    const decoded = jwtDecode(token);
+    // console.log(decoded)
+    const { Username, roles } = decoded.UserInfo;
+
+    isAdmin = roles.includes("Admin");
+    isAuthor = roles.includes("Author");
+
+    if (isAdmin) status = "Admin";
+    if (isAuthor) status = "Author";
+
+    return { Username, status, roles, isAdmin, isAuthor };
+  }
+
+  return { Username: "", roles: [], status };
+};
+
+export default useAuth;
