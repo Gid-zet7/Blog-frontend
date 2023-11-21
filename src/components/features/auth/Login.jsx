50e90a22a960106b9e2f8a1b962a09e9e07df089
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-21 01:25:23 +0000

    Updated login component to include login form to allow users enter their credentials to be sent to the server for assessment

--- src/components/features/auth/Login.jsx
+++ src/components/features/auth/Login.jsx
@@ -1,5 +1,99 @@
+import { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
+import { PulseLoader } from "react-spinners";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const userRef = useRef();
+  const errRef = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+      navigate("/dash/welcome");
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg("No server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(err.data?.message);
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleUserInput = (e) => setUsername(e.target.value);
+  const handlePasswordInput = (e) => setPassword(e.target.value);
+
+  const errClass = errMsg ? "errMsg" : "offscreen";
+
+  if (isLoading) return <PulseLoader color={"#BADA55"} />;
+
+  const content = (
+    <section id="login">
+      <div id="auth_header">
+        <h1>Login</h1>
+      </div>
+      <div>
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+        <form id="login_form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordInput}
+            required
+          />
+          <button>Sign In</button>
+        </form>
+        <div>
+          <Link to="/">Back to Home</Link>
+        </div>
+      </div>
+    </section>
+  );
+
+  return content;
 };
 
 export default Login;
