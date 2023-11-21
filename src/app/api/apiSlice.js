625587c1273fddadd68f38815e53df3939a767f5
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-21 01:09:50 +0000

    Updated apiSlice to set headers with the token and send refresh tokens

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,50 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials } from "../../components/features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:9000",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  // console.log(`args--${args}`);
+  // console.log(`api--${api}`);
+  // console.log(`extraOptions--${extraOptions}`); // {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    console.log("sending refresh token");
+
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+
+    console.log(refreshResult);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        refreshResult.error.data.message = "Your login has expired";
+      }
+      return refreshResult;
+    }
+  }
+  return result;
+};
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["Post", "User"],
   endpoints: (builder) => ({}),
 });
