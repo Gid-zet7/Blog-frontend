4f48fea9be4bbf8cc7feab7fa11380b64a16f44a
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-23 08:29:36 +0000

    Updated post component to include comments

--- src/components/features/posts/Post.jsx
+++ src/components/features/posts/Post.jsx
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 import { useGetPostsQuery } from "./postsApiSlice";
+import NewCommentForm from "../comments/NewCommentForm";
+import useAuth from "../../../hooks/useAuth";
+import CommentsList from "../comments/CommentsList";
 
-const Post = ({ postId }) => {
+const Post = ({ postId, commentId }) => {
+  const { Username } = useAuth();
   const { post } = useGetPostsQuery("postsList", {
     selectFromResult: ({ data }) => ({
       post: data?.entities[postId],
@@ -13,6 +17,7 @@ const Post = ({ postId }) => {
   });
 
   const navigate = useNavigate();
+  const [viewComment, setViewComment] = useState(false);
 
   if (post) {
     const created = new Date(post.createdAt).toLocaleString("en-US", {
@@ -22,20 +27,32 @@ const Post = ({ postId }) => {
     });
 
     const handleEdit = () => navigate(`/dash/posts/${postId}`);
+    const handleCommentView = () => {
+      setViewComment((prevState) => !prevState);
+    };
 
     return (
-      <div>
-        <h1>Title: {post.title} </h1>
-        <p>Author: {post.username}</p>
-        <img src={post.image.url} alt="something" />
-        <p>{post.body} </p>
-        <p>#{post.category} </p>
-        <p>Date: {created} </p>
-        {/* <p>{updated} </p> */}
-        <button onClick={handleEdit}>
-          <FontAwesomeIcon icon={faPenToSquare} />
-        </button>
-      </div>
+      <>
+        <div>
+          <h1>Title: {post.title} </h1>
+          <p>Author: {post.username}</p>
+          <img src={post.image.url} alt="something" />
+          <p>{post.body} </p>
+          <p>#{post.category} </p>
+          <p>Date: {created} </p>
+          <button onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </div>
+        <button onClick={handleCommentView}>View Comments</button>
+        {viewComment && (
+          <>
+            <h1>Comments</h1>
+            <NewCommentForm postId={postId} username={Username} />
+            <CommentsList postId={post.id} />
+          </>
+        )}
+      </>
     );
   } else return null;
 };
