a4860b9903f18290a15db7a4117d3d69adc947e8
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-25 21:24:19 +0000

    Public Post List displays the list of public posts on the public page

---
+++ src/components/features/posts/PublicPostsList.jsx
@@ -0,0 +1,46 @@
+import { useGetPubPostsQuery } from "./PublicPostsApiSlice";
+import PublicPost from "./PublicPost";
+import { PulseLoader } from "react-spinners";
+
+const PublicPostsList = () => {
+  // const sectionContainer = {
+  //   marginTop: "2.5rem",
+  // };
+  const {
+    data: posts,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPubPostsQuery("publicList", {
+    pollingInterval: 15000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <PulseLoader color={"#BADA55"} />;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message} </p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = posts;
+
+    // let filteredIds = ids.filter(
+    //   (postId) => entities[postId].username === "Gidzet"
+    // );
+
+    const postContent =
+      ids?.length &&
+      ids.map((postId) => <PublicPost key={postId} postId={postId} />);
+
+    content = <>{postContent}</>;
+
+    return content;
+  }
+};
+
+export default PublicPostsList;
