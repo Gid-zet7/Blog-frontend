c7fd96c3d0f2d35b55af7401c3c711de0ba02bcf
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-26 05:26:11 +0000

    Modified comment component

--- src/components/features/comments/Comment.jsx
+++ src/components/features/comments/Comment.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { memo, useEffect } from "react";
 
 import { useGetCommentsQuery } from "./CommentsApiSlice";
@@ -18,7 +18,11 @@ const Comment = ({ commentId, postId }) => {
     { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
   ] = useDeleteCommentMutation();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isDelSuccess) console.log("deleted succesfully");
+  }, [isDelSuccess]);
 
   if (comment) {
     const created = new Date(comment.createdAt).toLocaleString("en-US", {
@@ -28,6 +32,9 @@ const Comment = ({ commentId, postId }) => {
     });
 
     // const handleEdit = () => navigate(`/dash/posts/${commentId}`);
+    const errClass = isDelError ? "errmsg" : "offscreen";
+
+    const errContent = delerror?.data?.message ?? "";
 
     let content;
     if (comment.post === postId) {
@@ -35,14 +42,17 @@ const Comment = ({ commentId, postId }) => {
         await deleteComment({ id: comment.id });
       };
       content = (
-        <div>
-          <p>Author: {comment.username}</p>
-          <p>{comment.content} </p>
-          <p>Date: {created} </p>
-          <button onClick={onDeleteCommentClicked}>
-            <FontAwesomeIcon icon={faTrash} />
-          </button>
-        </div>
+        <>
+          <p className={errClass}>{errContent}</p>
+          <div>
+            <p>Author: {comment.username}</p>
+            <p>{comment.content} </p>
+            <p>Date: {created} </p>
+            <button onClick={onDeleteCommentClicked}>
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          </div>
+        </>
       );
     }
 
