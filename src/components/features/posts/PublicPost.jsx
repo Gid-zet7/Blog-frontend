b5cbedf052588bd023a53a0079658b50beb3fe9e
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-27 05:22:39 +0000

    Included icons and modified styles

--- src/components/features/posts/PublicPost.jsx
+++ src/components/features/posts/PublicPost.jsx
@@ -1,5 +1,7 @@
 import { memo } from "react";
 import { useGetPubPostsQuery } from "./PublicPostsApiSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const PublicPost = ({ postId }) => {
   const container = {
@@ -9,6 +11,7 @@ const PublicPost = ({ postId }) => {
     justifyContent: "center",
     padding: "1rem",
     alignItems: "center",
+    fontSize: ".8rem",
   };
   const { post } = useGetPubPostsQuery("publicList", {
     selectFromResult: ({ data }) => ({
@@ -29,18 +32,28 @@ const PublicPost = ({ postId }) => {
       const truncBody = post.body.substr(0, 200) + "...";
       content = (
         <div style={container}>
+          <div style={{ padding: "1rem" }}>
+            <h1>{post.title} </h1>
+          </div>
           <img
             src={post.image.url}
             alt="something"
             style={{ width: "90%", height: "100%" }}
           />
-          <p>Author: {post.username}</p>
 
-          <h1>{post.title} </h1>
-          <p>{truncBody} </p>
+          <div style={{ padding: "1rem 1rem 1rem 2rem" }}>
+            <p>{truncBody} </p>
+          </div>
+          <div style={{ display: "flex", gap: 8, padding: 7 }}>
+            <p>
+              <FontAwesomeIcon icon={faUser} /> {post.username}
+            </p>
+            <p>#{post.category} </p>
+          </div>
 
-          <p>#{post.category} </p>
-          <p>Date: {created} </p>
+          <p>
+            <FontAwesomeIcon icon={faCalendar} /> {created}
+          </p>
         </div>
       );
     }
