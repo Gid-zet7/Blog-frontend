cb980573d51a82fa8511c26019168ff20c980e73
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-27 05:32:19 +0000

    Modified footer content and added some styling

--- src/components/DashFooter.jsx
+++ src/components/DashFooter.jsx
@@ -20,11 +20,35 @@ const DashFooter = () => {
     );
   }
 
+  const footerStyles = {
+    display: "flex",
+    gap: "1rem",
+    position: "absolute",
+    bottom: 0,
+    color: "#fff",
+    width: "100vw",
+    padding: "2rem 2.5rem",
+    backgroundColor: "#000",
+  };
+
   const content = (
-    <footer>
-      {redirectBtn}
-      <p>Current User:{Username} </p>
-      <p>Status:{status} </p>
+    <footer style={footerStyles}>
+      <div>
+        <h6>{Username} </h6>
+        <h6>{status} </h6>
+        {redirectBtn}
+      </div>
+      <div className="about-footer">
+        <h4>About Bincika</h4>
+        <h6>About us</h6>
+        <h6>Our mission</h6>
+      </div>
+      <div className="contact-footer">
+        <h4>Contact us</h4>
+        <h6>Advertise</h6>
+        <h6>Whatsapp</h6>
+        <h6>Email</h6>
+      </div>
     </footer>
   );
   return content;
