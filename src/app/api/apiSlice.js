7e3367ff77c104d999262445dac6642a4cc8095a
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-28 04:34:29 +0000

    Ready to deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../components/features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:9000",
+  baseUrl: "https://blogposts-api-r2tf.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
--- src/app/store.js
+++ src/app/store.js
@@ -10,6 +10,6 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  devTools: false,
 });
 setupListeners(store.dispatch);
